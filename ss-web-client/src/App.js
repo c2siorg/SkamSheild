55385356801747151cc11aebd564a66d00ac4009
hachchi <161139489+hachchi@users.noreply.github.com>
2024-07-08 17:55:21 +0000

    feature: update app theme

--- ss-web-client/src/App.js
+++ ss-web-client/src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 
-function App() {
+import { ThemeProvider } from "@mui/material/styles";
+import LinearProgress from "@mui/material/LinearProgress";
+
+import COMPANY_LOGO from "./assests/companyLogo2.png";
+
+import { AuthContext, AuthProvider } from "./context/AuthContext/AuthContext";
+import { ScamDataProvider } from "./context/ScamDataContext/ScamDataContext";
+import { CustomThemeContext } from "./context/CustomThemeContext/CustomThemeContext";
+
+import {
+  App,
+  Signin,
+  NotFound,
+  Management,
+  Reviews,
+  ReviewContent,
+  Reports,
+} from "./containers";
+
+export default function AppRoutes() {
+  const { customTheme } = useContext(CustomThemeContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ThemeProvider theme={customTheme}>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Signin />} />
+            <Route path="/main">
+              <Route
+                path="/main/reviews"
+                element={<PrivateRoute component={<Reviews />} />}
+              />
+              <Route
+                path="/main/review-content/:contentTypeId"
+                element={<PrivateRoute component={<ReviewContent />} />}
+              />
+              <Route
+                path="/main/management"
+                element={<PrivateRoute component={<Management />} />}
+              />
+              <Route
+                path="/main/reports"
+                element={<PrivateRoute component={<Reports />} />}
+              />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </AuthProvider>
+      </ThemeProvider>
+    </Router>
   );
 }
 
-export default App;
+const PrivateRoute = ({ component }) => {
+  const { user, initializing } = useContext(AuthContext);
+
+  if (initializing) {
+    return (
+      <div style={{ width: "100%" }}>
+        <LinearProgress />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+            height: "99vh",
+          }}
+        >
+          <a href="https://www.flaticon.com/free-icons/scam" title="scam icons">
+            <img width={150} alt="companyLogo" id="logo" src={COMPANY_LOGO} />
+          </a>
+        </div>
+      </div>
+    );
+  }
+
+  // if (!user) {
+  //   return <Navigate to="/" />;
+  // }
+
+  return (
+    <ScamDataProvider>
+      <App>{component}</App>
+    </ScamDataProvider>
+  );
+};
